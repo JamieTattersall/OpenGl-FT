1353542a60621b545e2bd88a4dad24e1096742ac
627719@eastridingcollege.ac.uk <627719@eastridingcollege.ac.uk>
2025-02-12 04:05:28 +0000

    Finally got a triangle on my screen, Will update my onenote with how i did it and add comments in next push

--- Main.cpp
+++ Main.cpp
@@ -16,6 +16,16 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
 "}\n\0";
 
+//source for vertex 
+GLfloat vertices[] =
+{
+	-0.5f, -0.5f * float(sqrt(3)) / 3,0.0f,
+	0.5f, -0.5f * float(sqrt(3)) / 3,0.0f,
+	0.0f, 0.5f * float(sqrt(3)) / 3,0.0f,
+
+};
+
+
 
 int main()
 {
@@ -45,6 +55,48 @@ int main()
 	//  Specifying the viewport of OpenGL in the window
 	glViewport(0, 0, 800, 800);
 
+	//making vertx shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+
+
+
+
+
 	// Declaring the color
 	glClearColor(0.17f, 0.20f, 0.27f, 1.0f);
 
@@ -57,10 +109,26 @@ int main()
 	//  While window isnt closed run window -- Just makes it that the window constantly runs -- Other wise would just flash for a frame 
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.17f, 0.20f, 0.27f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
+
+
+
 		glfwPollEvents();
 	}
 
 	// Destroy the window and terminate glfw
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
